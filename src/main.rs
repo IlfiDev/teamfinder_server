0f0e7ec4e43c092e656e47984e4e2ae013defe0e
IlfiDev <ilyafillipov2@gmail.com>
2023-04-17 16:33:26 +0300

    add: actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-   println!("initial!"); 
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+struct AppState {
+   app_name: String,
+}
+
+
+#[get("/")]
+async fn index(data: web::Data<AppState>) -> String {
+   let app_name = &data.app_name;
+   format!("Hello {app_name}!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+   HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+   HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+   HttpServer::new(|| {
+      App::new()
+         .app_data(
+            web::Data::new(
+               AppState {
+                  app_name: String::from("Actix Web"),
+               }
+            )
+         )    
+         .service(index)
+         .service(echo)
+         .route("/hey", web::get().to(manual_hello))
+   })
+      .bind(("127.0.0.1", 8080))?
+   .run()
+      .await
+
 }
